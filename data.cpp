1e4fd83af658e148d0792eb7638715ef45e05347
Barsbold <barsbold01@users.noreply.github.com>
2026-04-07 12:31:40 +0800

    square class nemev

--- data.cpp
+++ data.cpp
@@ -49,12 +49,43 @@ public:
 // Square класс
 class Square : public Shape2D {
 private:
-    double side;
-
+    double side;       // талын урт
+ 
+    // 4 оройн координатууд (x,y - зүүн дээд орой Shape2D-с удамшсан)
+    double x2, y2;     // баруун дээд орой
+    double x3, y3;     // баруун доод орой
+    double x4, y4;     // зүүн доод орой
+ 
 public:
-    void input();      
-    double area();     
-    double perimeter();
+    // Оролт: зүүн дээд оройн координат болон талын урт авна,
+    // бусад гурван оройг тооцоолж хадгална
+    void input() {
+        setName("Квадрат");
+        cout << "Квадратын зүүн дээд оройн x координат: ";
+        cin >> x;
+        cout << "Квадратын зүүн дээд оройн y координат: ";
+        cin >> y;
+        cout << "Квадратын талын урт: ";
+        cin >> side;
+ 
+        // Зүүн дээд орой  (x,  y) - Shape2D-с удамшсан
+        // Баруун дээд орой (x2, y2) - x чиглэлд side-аар шилжсэн
+        x2 = x + side;  y2 = y;
+        // Баруун доод орой (x3, y3) - x болон y чиглэлд side-аар шилжсэн
+        x3 = x + side;  y3 = y - side;
+        // Зүүн доод орой  (x4, y4) - y чиглэлд side-аар шилжсэн
+        x4 = x;         y4 = y - side;
+    }
+ 
+    // Талбай = тал * тал
+    double area() {
+        return side * side;
+    }
+ 
+    // Периметр = 4 * тал
+    double perimeter() {
+        return 4 * side;
+    }
 };
 
 
@@ -63,6 +94,7 @@ public:
 class Triangle : public Shape2D {
 private:
     double side;
+    
 
 public:
     void input();      
